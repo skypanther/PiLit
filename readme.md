3b6676b740ca8c4fff3e8f32ea893be915e7a4b9
Tim Poulsen <tim@skypanther.com>
2016-11-11 16:44:04 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Christmas Lights Controller
+
+RaspberryPi based Christmas lights controller system using a Sainsmart 16-channel relay board.
+
+Very much a work-in-progress. Even when "done" this will be hacker's tool, not some slick GUI-based app.
+
+Real instructions will follow.
---
+++ tester.js
@@ -0,0 +1,69 @@
+/*
+
+Test your show with testshow.js
+
+*/
+
+var _ = require("lodash"),
+	axel = require('axel'),
+	clear = require("clear"),
+	colors = require("colors"),
+	fields = require("fields"),
+	fs = require("fs"),
+	path = require("path");
+
+var boxWidth = 4,
+	boxHeight = 2,
+	colors = ["#C7C101", "#E39E03", "#F6780F", "#FE5326", "#FB3244", "#ED1868", "#D5078E", "#B601B3", "#9106D3", "#6B16EC", "#472FFA", "#2850FE", "#1175F7", "#039BE5", "#01BECA", "#0ADCA8"],
+	left = 2;
+
+axel.clear();
+
+var filePath = path.join('shows', 'Christmas show 2016');
+var showFile = fs.readFileSync(filePath, 'utf8'),
+	show = JSON.parse(showFile);
+
+function drawRow(arr) {
+	colors.forEach(function (color, index) {
+		var rgb = hexToRgb(color);
+		if (arr[index]) {
+			axel.bg(rgb.r, rgb.g, rgb.b);
+		} else {
+			axel.bg(0, 0, 0);
+		}
+		axel.box(left + index * 5, 4, boxWidth, boxHeight);
+	});
+}
+
+var i = 0,
+	interval = show.interval;
+
+var looper = setInterval(function () {
+	if (i < show.show.length) {
+		drawRow(show.show[i]);
+		i++;
+	} else {
+		clearInterval(looper);
+		looper = undefined;
+		axel.cursor.restore();
+	}
+}, interval);
+
+
+
+
+function hexToRgb(hex) {
+	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+	// from http://stackoverflow.com/a/5624139/292947
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+		return r + r + g + g + b + b;
+	});
+
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result ? {
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16)
+	} : null;
+}
