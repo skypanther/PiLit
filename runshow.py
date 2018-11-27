b378f9656f90a73f3993c2049eaffc6f9cb0db3b
Tim Poulsen <tim@skypanther.com>
2018-11-26 20:55:39 -0500

    show update

--- runshow.py
+++ runshow.py
@@ -42,7 +42,7 @@ from gpiozero import LED
 
 # CONFIGURE THESE VARIABLES TO SUIT YOUR ACTUAL SETUP
 # don't use pin 8 on a Pi B+ as it's the "hard drive light" pin
-board_pins = [3, 5, 7, 26, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24]
+board_pins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24]
 
 # DON'T CHANGE THESE VARIABLES
 midnight = datetime.today().replace(
