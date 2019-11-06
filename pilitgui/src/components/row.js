28e4875bac55cd6300cb79b73d9f9e0291113597
Tim Poulsen <tim@skypanther.com>
2019-11-05 21:27:05 -0500

    styling changes

--- pilitgui/src/components/row.js
+++ pilitgui/src/components/row.js
@@ -7,6 +7,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Button from 'react-bootstrap/Button';
 
+import leaping_arch from '../../public/images/leaping_arch.jpg';
+
 class Row extends Component {
   constructor(props) {
     super(props);
@@ -17,14 +19,29 @@ class Row extends Component {
   }
 
   render() {
-    var rowTitle = "Pixel Tree 3";
+    var rowTitle = "Arch 1";
     return (
-      <div className="row-wrapper">
-        <div className="row-title">{rowTitle}</div>
-        <div className="row-inner-wrapper">
-          <PixelNode />
+      <div className="row-outer-wrapper">
+        <div className="row-wrapper">
+          <div className="row-title"><div className="row-title-text">{rowTitle}</div></div>
+          <div className="row-image-wrapper" id="rowImage"><img src={leaping_arch} className="row-image" /></div>
+          <div id="rowWrapper" className="row-inner-wrapper">
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+            <PixelNode />
+          </div>
+          <div className="button-wrapper">
+            <Button variant="light"><FontAwesomeIcon icon={faPlusCircle} onClick={() => { this.handleAddNode() }} /></Button>
+          </div>
         </div>
-        <Button variant="outline-light" className="add-node-button"><FontAwesomeIcon icon={faPlusCircle} onClick={() => { this.handleAddNode() }} /></Button>
       </div>
     )
   }
