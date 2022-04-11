a634bfb69748be2d03f32d86d688d890d28bbeb5
Tim Poulsen <tim@skypanther.com>
2022-04-10 21:31:55 -0400

    move config to separate file

--- nodes/pixel_node/pixel_node.ino
+++ nodes/pixel_node/pixel_node.ino
@@ -7,9 +7,14 @@
   License: MIT
 */
 
-// #define FASTLED_ESP8266_RAW_PIN_ORDER
-// #define FASTLED_ESP8266_RAW_PIN_ORDER
-#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+// XXXXXXXXXXXXXXXXXXXX
+// GENERALLY, ALL CONFIGURATION CAN BE DONE IN THE config.h FILE
+// XXXXXXXXXXXXXXXXXXXX
+
+// EXCEPT YOU MIGHT NEED TO SWAP WHICH OF THESE IS COMMENTED OUT
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+//#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+
 
 #include <ArduinoOTA.h>
 #include <ESP8266WiFi.h>
@@ -22,44 +27,12 @@
 #include <WiFiServer.h>
 #include <WiFiServerSecure.h>
 #include <WiFiUdp.h>
-
 #include <string>
 #include <unordered_map>
-
+#include "config.h"
 #include "utils.h"
+
 #define MAX_MESSAGE_LENGTH 128
-
-// CHANGE THESE TO MATCH THE PIXEL TYPE AND SPECIFICS OF YOUR SETUP
-#define LED_TYPE WS2812B
-#define DATA_PIN 5
-// #define CLOCK_PIN 4  // uncomment if using LEDs with a clock line, such as
-// APA102C
-#define NUM_LEDS 100
-#define COLOR_ORDER RGB
-#define BRIGHTNESS 100
-// #define MAX_MILLIAMPS 8000  // 8 amps
-
-// ------> CONFIGURE THESE VARIABLES TO MATCH YOUR SETUP  <------
-char *hostname = "teststrip";  // The hostname of this device
-String topics[] = {           // Create an array of topics to subscribe to
-    "all",                    // add as many topics as necessary
-    "tests", "teststrip"};
-char *brokerHostname = "northpole.local";  // or "192.168.1.6";  // Hostname/IP
-                                           // address of the MQTT broker
-char *ssid = "WIFI_NETWORK_SSID";
-char *password = "PASSWORD";
-char *ota_password = "PASSWORD";  // password for OTA updates
-
-int utcOffsetInHours = -5;  // EST
-
-// Some base delay times
-uint16_t loopDelay = 10;  // Time (ms) between calls to animation function
-uint16_t holdTime = 50;   // Time (ms) delay at the end of some back-n-forth
-                          // functions (e.g. bounce)
-// Repeat the animations. If false, they complete once then stop
-bool repeat = true;
-
-// XXXXXXXXXX  DON'T CHANGE ANYTHING BELOW THIS IN THIS FILE  XXXXXXXXXX
 CRGB leds[NUM_LEDS];
 
 WiFiClient wifiClient;
